a8bb2bb5c49a79790f0c219f608704b190a0ace3
siddharthpadki <siddharth.padki@btinternet.com>
2018-11-21 19:22:03 +0000

    Add files via upload

--- HolidayResort1/HolidayResort1/Models/HolidayPlanner.cs
+++ HolidayResort1/HolidayResort1/Models/HolidayPlanner.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.Net.Http;
 
 namespace HolidayResort1.Models
 {
     public class HolidayPlanner
     {
-        public HolidayPlanner() { }
+        //public HolidayPlanner() { }
 
         public int HolidayPlannerID { get; set; }
-        //public string Images { get; set; }       
+        public string Images { get; set; }       
         public string Country { get; set; }
         public string Resort { get; set; }
         public string Profile { get; set; }
         public int Like { get; set; }
         public int Dislike { get; set; }
+    }
+
+    public class HolidayDBContext : DbContext
+    {
+        public DbSet<HolidayPlanner> HolidayPlanners { get; set; }
     }
 }
